2ee7214a4a1e564bc075452d07aa8af4aa569af8
Ubuntu <ubuntu@ip-172-31-19-201.ap-northeast-2.compute.internal>
2018-11-09 11:50:16 +0000

    jh

---
+++ jhweb/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var server = app.listen(3516, function(){
+	console.log("Express server has started on port 3516")
+})
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+app.get('/',function(req, res){
+	res.render('user/login.html');
+});
+app.get('/signup', function(req, res){
+	res.render('user/signup.html');
+});
+
+app.post('/login',function(req, res){
+	var user_id = req.body.id;
+	var user_pw = req.body.password;
+t
+	var query = 
