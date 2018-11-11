a0486194a88eea8c4010cd32006da017b651894a
suyeon <77sy777@gmail.com>
2018-11-11 19:50:10 +0900

    Update syweb/app.js

--- syweb/app.js
+++ syweb/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000");
