e579a96e3deecd63758d650aa625d79e3d2f49be
suyeon <77sy777@gmail.com>
2018-11-11 19:40:34 +0900

    test

--- syweb/app.js
+++ syweb/app.js
@@ -1,17 +1,25 @@
-var http = require('http');
 var express = require('express');
 var app = express();
+var http = require('http');
+var bodyParser = require('body-parser');
+
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000");
 })
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.set('views', __dirname + '/views');
 app.set('views engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(req, res){
-    res.render('/index.html');
+    res.render('./index.html');
 });
 app.get('/login', function(req, res){
-    
+    res.render('./login.html');
 });
 app.get('/signup', function(req, res){
-    res.render('/signup.html');
-;});
+    res.render('./signup.html');
+});
