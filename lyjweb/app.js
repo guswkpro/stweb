8eae4ebb4d23439590f13fd68cd3d279ac543f38
calivan <kogr2378@naver.com>
2018-11-11 15:49:13 +0900

    lyj

--- lyjweb/app.js
+++ lyjweb/app.js
@@ -6,5 +6,5 @@ var server = app.listen(3000, function(){
 app.set('views engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.get('/', function(req, res){
-    res.render('./LoginForm.html');
+    res.send('hello');
 });
