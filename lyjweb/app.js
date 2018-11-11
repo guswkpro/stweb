e99052ea725a1bdc3d2c27dbea10f3fcf0129402
calivan <kogr2378@naver.com>
2018-11-11 14:25:35 +0900

    lyj

--- lyjweb/app.js
+++ lyjweb/app.js
@@ -1,45 +1,11 @@
+
 var express = require('express');
-var http = require('http');
-var controller = require('./controller/logincontroller');
 var app = express();
 var server = app.listen(3000, function(){
-	console.log("Express server has started on port 3000")
-});
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}));
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+    console.log("Express server has started on port 3000");
+})
+app.set('views engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-
-app.get('/app.js',function(req,res){
-    res.sned('hello world');
+app.get('/', function(req, res){
+    res.render('/index.html');
 });
-app.get('/LoginForm', function(req, res){
-	res.render('LoginForm.html');
-});
-app.get('/userprogile',function(res,req){
-    res.render('user.html')
-});
-
-app.post('/LoginForm',function(req, res){
-	
-    var req_mem_id = req.body.id;
-    var req_mem_pw = req.body.password;
-    
-    controller.login(req_mem_id,req_mem_pw,function(result){
-        res.json({
-            "RESULT":result
-        });
-    });
-
-});
-
-
-app.get('/JoinForm', function(req, res){
-	res.render('JoinForm.html');
-});
-
-
-
-
--- lyjweb/dao/index.js
+++ lyjweb/dao/index.js
@@ -1,15 +1,29 @@
-var mysql = require('mysql');
-var client mysql.createConnection({
-    host : 'calivan.ctk1nynf5quj.us-east-2.rds.amazonaws.com',
-    port : 3006,
-    user : 'calivan',
-    password : 'youngjun423!',
-    database : 'calivan'
-});
+var mysql      = require('mysql');
+var db = mysql.createConnection({
+    host     : 'lhttp://13.58.230.106',
+    user     : 'suyeon',
+    password : 'suyeondb',
+    database : 'sydb'
+  });
+    db.connect();
 
-export.login = function(id,pw,callback){
-    client.query('SELECT * FROM calivan.users_table where user_id=? AND user_password=?',[id,pw],function(error,result,fields){
-        console.log(result);
-        callback(error,result);
+    module.exports = db;
+exports.login = function(req, res){
+    var id = req.body.l_id;
+    var pw = req.body.l_pw;
+    db.query(`SElECT count(*) cnt FROM user_table WHERE user_id=? and user_pw=?`, [id, pw], function(err1, rows){
+        var cnt=rows[0].cnt;
+
+        if(cnt===1){
+            dq.query(`SELECT * FROM user_table WHERE user_id=?`, [id], function(err2, user){
+                if(err2){
+                    console.log('err');
+                }
+                res.render('./index',{
+                     id: user.user_id,
+                    name: user.user_name
+                 });
+            });
+        }
     });
-};
+}
