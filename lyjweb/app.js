d9e2fdd22b37f0f56a4f15686f244c0e30b28020
calivan <kogr2378@naver.com>
2018-11-11 16:01:16 +0900

    lyj

--- lyjweb/app.js
+++ lyjweb/app.js
@@ -5,6 +5,24 @@ var server = app.listen(3000, function(){
 })
 app.set('views engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(req, res){
-    res.render("./LoginForm.html");
+    res.render('./LoginForm.html');
+});
+
+app.get('/Join',function(req,res){
+    res.render('./JoinForm.html');
+})
+
+app.post('/LoginForm',function(req, res){
+	
+    var req_mem_id = req.body.id;
+    var req_mem_pw = req.body.password;
+    
+    controller.login(req_mem_id,req_mem_pw,function(result){
+        res.json({
+            "RESULT":result
+        });
+    });
+
 });
