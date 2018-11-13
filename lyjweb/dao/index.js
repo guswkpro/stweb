4bbcce4bd50a0de23cc3d8737a953b25fa73a74f
calivan <kogr2378@naver.com>
2018-11-14 02:32:57 +0900

    1114가입시간 수정중

--- lyjweb/dao/index.js
+++ lyjweb/dao/index.js
@@ -14,9 +14,34 @@ var db = mysql.createConnection({
       });
   };
 
+  function getTimeStamp() {
+    var d = new Date();
+    var s =
+        leadingZeros(d.getFullYear(), 4) + '-' +
+        leadingZeros(d.getMonth() + 1, 2) + '-' +
+        leadingZeros(d.getDate(), 2) + ' ' +
+
+        leadingZeros(d.getHours(), 2) + ':' +
+        leadingZeros(d.getMinutes(), 2) + ':' +
+        leadingZeros(d.getSeconds(), 2);
+
+    return s;
+    }
+
+    function leadingZeros(n, digits) {
+    var zero = '';
+    n = n.toString();
+
+    if (n.length < digits) {
+        for (i = 0; i < digits - n.length; i++)
+        zero += '0';
+    }
+    return zero + n;
+    }
+
   exports.join = function(id,pw,nick,email,callback){
-        
-      db.query('INSERT INTO users_table(user_id,user_password,user_nickname,user_email,user_signdate,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,?,?,?,?,?)',[id,pw,nick,email,NOW(),NOW(),null,0,null],function(error,result,fields){
+      var sign_data= getTimeStamp();
+      db.query('INSERT INTO users_table(user_id,user_password,user_nickname,user_email,user_signdate,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,?,?,?,?,?)',[id,pw,nick,email,sign_data,sign_data,null,0,null],function(error,result,fields){
           console.log(result);
            callback(error,result);
       });
