71f67701711e4d45406ec166fe494b8612f766e0
suyeon <77sy777@gmail.com>
2018-11-13 21:51:10 +0900

    11 /13 + signup

--- syweb/dao/index.js
+++ syweb/dao/index.js
@@ -29,7 +29,7 @@ exports.login = function(l_id, l_pw, callback){
     });
 }
 exports.signup = function(s_id, s_pw, s_name, s_email, s_address, s_mobile, s_birth, callback){
-    db.query(`INSERT INTO users_table (user_id, user_pw, user_name, user_email, user_address, user_mobile, user_birth, user_join_date, user_profile_path, user_point, user_access_date, user_witdraw) VALUES (?, ?, ?, ?, ?, ?, ?, NOW(), NULL, 0, NOW(), 0)`,[s_id, s_pw, s_name, s_email, s_address, s_mobile, s_birth],
+    db.query(`INSERT INTO users_table (user_id, user_pw, user_name, user_email, user_address, user_mobile, user_birth, user_join_date, user_profile_path, user_point, user_access_date, user_witdraw) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), 0, 0, NOW(), 0)`,[s_id, s_pw, s_name, s_email, s_address, s_mobile, s_birth],
         function(err, result){
             if(err){
                 console.error('err', err);
