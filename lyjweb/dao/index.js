746d509582f667996880a8b27a76626efc4ec3cf
calivan <kogr2378@naver.com>
2018-11-14 01:16:19 +0900

    1114

--- lyjweb/dao/index.js
+++ lyjweb/dao/index.js
@@ -18,7 +18,7 @@ var db = mysql.createConnection({
         //var signtime= new Date().toLocaleDateString;
       db.query('INSERT INTO users_table(user_id,user_password,user_nickname,user_email) VALUES(?,?,?,?)',[id,pw,nick,email],function(err,result,fields){
           console.log(result);
-          callback(error,result);
+         // callback(error,result);
       });
   }
 
