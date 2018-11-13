8420337b5c46fe79c5cb348f968b1f83669f3247
calivan <kogr2378@naver.com>
2018-11-13 17:21:32 +0900

    1113

--- lyjweb/controller/logincontroller/index.js
+++ lyjweb/controller/logincontroller/index.js
@@ -1,11 +1,11 @@
 var dao =require('../../dao/index.js');
-var ansync = require('async');
+var async = require('async');
 
 
 //로그인 위한 객체 생성..?
 exports.login = function(id,pw,callback){
     //비동기함수를 순차적으로 실행하지만 각 작업의 결과를 다음 작업으로 넘길때
-    ansync.waterfall([
+    async.waterfall([
         function(nextcallback){
             dao.login(id,pw,nextcallback);
         }
@@ -24,7 +24,7 @@ exports.login = function(id,pw,callback){
 }
 
 exports.join = function(id,pw,nick,email,callback){
-    ansync.waterfall([
+    async.waterfall([
         function(nextcallback){
             dao.join(id,pw,nick,email,nextcallback);
         }
@@ -45,7 +45,7 @@ exports.getUserProfile = function(req,res){
         var count =0;
         var list=[];
 
-        ansync.whilst(function(){
+        async.whilst(function(){
             return count<result.length;
         }, function(callback){
             list.push(result[cout]);
