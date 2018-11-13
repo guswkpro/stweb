2203037c5676a4c4a63da62903e6f198aa3935cc
calivan <kogr2378@naver.com>
2018-11-14 01:48:46 +0900

    1114

--- lyjweb/dao/index.js
+++ lyjweb/dao/index.js
@@ -15,38 +15,18 @@ var db = mysql.createConnection({
   };
 
   exports.join = function(id,pw,nick,email,callback){
-        //var signtime= new Date().toLocaleDateString;
-      db.query('INSERT INTO users_table(user_id,user_password,user_nickname,user_email) VALUES(?,?,?,?)',[id,pw,nick,email],function(error,result,fields){
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+        var signdate=mm+'/'+dd+'/'+yyyy;
+      db.query('INSERT INTO users_table(user_id,user_password,user_nickname,user_email,user_signdate) VALUES(?,?,?,?,?)',[id,pw,nick,email,signdate],function(error,result,fields){
           console.log(result);
            callback(error,result);
       });
   }
 
-  exports.getuser = (callback) => {
+  exports.getuser = function(callback) {
       db.query('select * from users_table', callback);
   }
 
-/*
-    db.connect();
-
-    module.exports = db;
-exports.login = function(req, res){
-    var id = req.body.l_id;
-    var pw = req.body.l_pw;
-    db.query(`SElECT count(*) cnt FROM user_table WHERE user_id=? and user_pw=?`, [id, pw], function(err1, rows){
-        var cnt=rows[0].cnt;
-
-        if(cnt===1){
-            dq.query(`SELECT * FROM user_table WHERE user_id=?`, [id], function(err2, user){
-                if(err2){
-                    console.log('err');
-                }
-                res.render('./index',{
-                     id: user.user_id,
-                    name: user.user_name
-                 });
-            });
-        }
-    });
-}
-*/
