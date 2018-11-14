61182ec4c5ae27f2b0f559c9052a6a6e94d69709
calivan <kogr2378@naver.com>
2018-11-14 09:38:46 +0900

    1114 가입시간

--- lyjweb/dao/index.js
+++ lyjweb/dao/index.js
@@ -42,8 +42,8 @@ var db = mysql.createConnection({
     */
   exports.join = function(id,pw,nick,email,callback){
      // var data= getTimeStamp();
-      var sign_data=(new Date()).toISOString().split(/[T\.]/).slice(0,2).join(' ');
-      db.query('INSERT INTO users_table(user_id,user_password,user_nickname,user_email,user_signdate,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,?,?,?,?,?)',[id,pw,nick,email,sign_data,sign_data,null,0,null],function(error,result,fields){
+      var sign_data=new Date().toISOString().slice(0, 19).replace('T', ' ');
+      db.query('INSERT INTO users_table(user_id,user_password,user_nickname,user_email,user_sign_date,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,?,?,?,?,?)',[id,pw,nick,email,sign_data,sign_data,null,0,null],function(error,result,fields){
           console.log(result);
            callback(error,result);
       });
