1064452e19a1109bd49d89c67bacdf8697e1fbd4
calivan <kogr2378@naver.com>
2018-11-14 11:35:41 +0900

    1114

--- lyjweb/dao/index.js
+++ lyjweb/dao/index.js
@@ -13,9 +13,9 @@ var db = mysql.createConnection({
           console.log(result);
           callback(error,result);
       });
-      db.query('UPDATE FROM users_table SET user_recent_date = ? WHETE user_id= ?',[recent_date,id],function(error,result,fields){
-          console.log(result);
-      });
+     // db.query('UPDATE FROM users_table SET user_recent_date = ? WHETE user_id= ?',[recent_date,id],function(error,result,fields){
+     //     console.log(result);
+     // });
   };
   /*
 
@@ -47,7 +47,7 @@ var db = mysql.createConnection({
   exports.join = function(id,pw,nick,email,callback){
      // var data= getTimeStamp();
       var sign_data=new Date().toISOString().slice(0, 19).replace('T', ' ');
-      db.query('INSERT INTO users_table(user_id,user_password,user_nickname,user_email,user_sign_date,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,?,?,?,?,?)',[id,pw,nick,email,sign_data,sign_data,null,0,null],function(error,result,fields){
+      db.query('INSERT INTO users_table(user_id,user_password,user_nickname,user_email,user_sign_date,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,?,?,?,?,?)',[id,pw,nick,email,NOW(),sign_data,null,0,null],function(error,result,fields){
           console.log(result);
            callback(error,result);
       });
