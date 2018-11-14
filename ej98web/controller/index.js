7c4a0e5654626d713000a57011b1325ddd099d57
Ubuntu <ubuntu@ip-172-31-27-5.ap-northeast-2.compute.internal>
2018-11-14 12:18:28 +0000

    11/14 login db연동

--- ej98web/controller/index.js
+++ ej98web/controller/index.js
@@ -1,27 +1,43 @@
-var mysql = require("mysql");
+var dao = require('../dao/index.js');
+var async = require('async');
 
-var conneection = mydb.createConnection({
-	host : 'localhost',
-	port : 3000,
-	user : 'root',
-	password : 'root',
-	database : 'mydb',
-});
 
-connection.connect();
 
-connection.query('select name from user where id="' + id + '"',function(err,rows){
-	if(err) throw err;
+exports.login = function(uid,upw, callback) {
+ 
+	async.waterfall([
+		function(nextCallback){
+			dao.login(uid,upw,nextCallback);
+		}
+	], function(error, result){
+		if(error){
+			console.log(error);
+			callback("0");
+		}
+		else if(result.length == 0) {
+			callback("2");
+		}
+		else{
+			callback("1");
+		}
+	});
 
-	if(rows[0]){
-		responseData.result = "ok";
-		responseData.id = rows[0].id;
-	}
-	else{
-		responseData.result = "none";
-		responseData.id = "";
-	}
-	res.json(responseData);
-})
+};
 
+
+exports.getUserProfile = function(req, res) {
+	var uid = req.param('id');
+	
+	dao.getUserProfile(uid, function(result){
+		var count = 0;
+		var list = [];
+		
+	
+	res.json({
+	
+	   memlist : list
+	});
+
+      });
+};
 
