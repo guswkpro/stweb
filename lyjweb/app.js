101d099c6b153293c2929672de243fa2e0867bb0
calivan <kogr2378@naver.com>
2018-11-15 01:32:24 +0900

    1115 가입문제수정중

--- lyjweb/app.js
+++ lyjweb/app.js
@@ -55,6 +55,11 @@ app.post('/JoinForm',function(req, res){
     var req_email = req.body.email;
 
     controller.join(req_id,req_pw,req_nick,req_email,function(result){
-        res.redirect('/');
+        if(result==1){        
+            res.redirect('/');
+        }
+        else{
+            alert("다시 작성하세요");
+        }
     });
 });
