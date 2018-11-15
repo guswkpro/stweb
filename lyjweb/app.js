ee6e5cd63063281bad5f47e866a8f56a0929c90e
calivan <kogr2378@naver.com>
2018-11-15 23:21:20 +0900

    1115

--- lyjweb/app.js
+++ lyjweb/app.js
@@ -55,8 +55,6 @@ app.post('/JoinForm',function(req, res){
     var req_email = req.body.email;
 
     controller.join(req_id,req_pw,req_nick,req_email,function(result){
-        if(result=="1"){
-            res.render('/LoginForm'); 
-        }
+        res.render('/LoginForm'); 
     });
 });
