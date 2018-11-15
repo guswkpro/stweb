6641890e89ad8e00e1b5c202604b2e0896136ccb
calivan <kogr2378@naver.com>
2018-11-15 23:22:53 +0900

    1115

--- lyjweb/dao/index.js
+++ lyjweb/dao/index.js
@@ -9,7 +9,7 @@ var db = mysql.createConnection({
 
   exports.login=function(id,pw,callback){
       var recent_date =new Date().toISOString().slice(0, 19).replace('T', ' ');
-      db.query('SELECT * FROM users_table where user_id=? AND user_password =?',[id,pw],function(error,result,fields){
+      db.query('SELECT * FROM calivan.users_table where user_id=? AND user_password =?',[id,pw],function(error,result,fields){
           console.log(result);
           callback(error,result);
       });
@@ -35,7 +35,7 @@ var db = mysql.createConnection({
   exports.join = function(id,pw,nick,email,callback){
      // var data= new Date().toLocaleDateString('se').replace(/\D/g, '')
       var sign_date=new Date().toISOString().slice(0, 10).replace('T', ' ');
-      db.query('INSERT INTO users_table(user_id,user_password,user_nickname,user_email,user_sign_date,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,?,?,?,?,?)',[id,pw,nick,email,sign_date,sign_date,null,0,null],function(error,result,fields){
+      db.query('INSERT INTO calivan.users_table(user_id,user_password,user_nickname,user_email,user_sign_date,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,?,?,?,?,?)',[id,pw,nick,email,sign_date,sign_date,null,0,null],function(error,result,fields){
           console.log(result);
            callback(error,result);
       });
