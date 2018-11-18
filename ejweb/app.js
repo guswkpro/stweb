1bd7f9e1d259fa96a435919f300556cdfe284de9
mareun <44865268+mareun@users.noreply.github.com>
2018-11-19 00:35:04 +0900

    11/19

--- ejweb/app.js
+++ ejweb/app.js
@@ -1,22 +1,41 @@
 var express = require('express');
-var http = require('http');
+var app = module.exports = express();
+//var http = require('http');
 var logincontroller = require('./controller/logincontroller');
-var app = express();
-var bodyParser = require('body-parser');
-//var session = require(express-session); // express의 미들웨어 사용
-//var MysqlStore = require('session-mysql-store')(session);
+var bodyParser = require('body-parser'); //POST 방식 전송을 위해서 필요
+var session = require(express-session); // express의 미들웨어 사용
+var MysqlStore = require('session-mysql-store')(session);
 //npm install express-mysql-session --save //Mysql을 Express의 Session store로 사용
+
 
 
 var server = app.listen(3000, function(){
 	console.log("Express server has started on port 3000")
 })
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));//미들웨어 등록부분
+app.use(bodyParser.json());
+
 
 app.set('views', __dirname + '/views');
 app.set('view engin', 'ejs');
 app.engin('html', require('ejs').renderFile);
+
+app.use(session({
+	secret: '%$#secret%#$',	//쿠키를 임의로 변조하는것을 방지, 세션을 암호화
+	resave : false, //세션을 언제나 저장할지(변경하지않아도)정하는 값, false로 하는것을 권장
+	saveUninitialized: true, //세션이 저장되기 전에 uninitialized상태로 미리 만들어서 저장
+	store: new MysqlStore({
+				host: 'jungedbinstance.colngshlieoc.us-east-2.rds.amazonaws.com',
+				port: 3306,
+                user: 'root',
+                password: '0000',
+                database: 'JungeDBInstance'
+	})
+
+
+}))
+//resave 세션아이디를 접속할때마다 발급하지 않는다.
 
 app.get('/', function(req, res){
 	res.render('login.html');
