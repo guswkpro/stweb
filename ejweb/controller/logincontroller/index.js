6a11a23c5d3e6e58fc3cff59a39a79c9e6c8ae5c
mareun <44865268+mareun@users.noreply.github.com>
2018-11-19 20:35:06 +0900

    11/19

--- ejweb/controller/logincontroller/index.js
+++ ejweb/controller/logincontroller/index.js
@@ -16,8 +16,3 @@ exports.login = function(id, pw, callback){
 		}
 	});
 
-//exports.join = function(id, pw, mail, nickname, tel){}; //회원가입
-//1. 회원가입시 정보를 사용자에게서 받는다.
-//2. 만약 정보기입이 잘못되었다면? 에러를 낸다. -> 다시 회원가입기입 해야한다.
-//3. 사용자에게서 받은 정보를 저장한다.
-
