050665c0e71e93e1d072681ad817a32a49af8b38
calivan <kogr2378@naver.com>
2018-11-21 22:23:16 +0900

    1121

--- lyjweb/dao/index.js
+++ lyjweb/dao/index.js
@@ -1,7 +1,6 @@
 var mysql      = require('mysql');
 var db = mysql.createConnection({
     host     : 'calivan.ctk1nynf5quj.us-east-2.rds.amazonaws.com',
-    port : 3306,
     user     : 'calivan',
     password : 'youngjun423!',
     database : 'calivan'
@@ -41,8 +40,7 @@ var db = mysql.createConnection({
   exports.join = function(id,pw,nick,email,callback){
      // var data= new Date().toLocaleDateString('se').replace(/\D/g, '')
       //var sign_date=new Date().toISOString().slice(0, 10).replace('T', ' ');
-      now = db.query('SELECT NOW()');
-      db.query('INSERT INTO calivan.users_table(user_id,user_password,user_nickname,user_email,user_sign_date,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,?,?,?,?,?)',[id,pw,nick,email,now,now,null,0,null],function(error,result,fields){
+      db.query('INSERT INTO calivan.users_table(user_id,user_password,user_nickname,user_email,user_sign_date,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,NOW(),NOW(),0,0,0)',[id,pw,nick,email],function(error,result,fields){
           console.log(result);
            callback(error,result);
       });
